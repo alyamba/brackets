ded8284005f99c40419bd750ffc5bc14ccc1c004
alyamba <logunionok@gmail.com>
2022-08-09 14:46:03 +0300

    fix: add new function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+  if (str.length % 2 !== 0) {
+    return false;
+  } else {
+    const pair = bracketsConfig.map((currentValue) => currentValue.join(""));
+
+    for (let i = 0; i < pair.length; i++) {
+      if (str.includes(pair[i])) {
+        str = str.replace(pair[i], "");
+        i = -1;
+      }
+    }
+
+    return str === "";
+  }
+};
